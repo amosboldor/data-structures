e2c8a50eb4ec8003726e853dfd497ae04fa4a260
Amos Boldor <amosboldor@gmail.com>
2016-12-12 13:19:07 -0800

    add setup.py and tox.ini

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""Setup mailroom madness module."""
+
+
+from setuptools import setup
+
+setup(
+    name="Linked Lists",
+    description="A Implementation a Singly-Linked List in Python",
+    version=0.1,
+    author=["Claire Gatenby", "Amos Boldor"],
+    licencse="MIT",
+    package_dir={'': 'src'},
+    py_modules=["linked_list"],
+    extras_require={
+        "test": ["pytest", "pytest-cov", "tox"]
+    }
+)
