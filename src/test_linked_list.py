64c408ba819792e3cb7baf6faee85b4f2d2c0e4c
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-12 15:00:50 -0800

    added testing function for push method

--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -14,6 +14,15 @@ LIST_DATA = [
 @pytest.mark.parametrize("n", LIST_DATA)
 def test_initiate_linkedlist(n):
     """Test if attributes in linked list match what is expected."""
-    from linked_list import LinkedList
-    example = LinkedList(n)
+    from linked_list import Node
+    example = Node(n)
     assert example.data == n
+
+
+@pytest.mark.parametrize("n", LIST_DATA)
+def test_push(n):
+    """Test push updates head of the linked list."""
+    from linked_list import LinkedList
+    linked_list = LinkedList()
+    linked_list.push(n)
+    assert linked_list.head.data == n
