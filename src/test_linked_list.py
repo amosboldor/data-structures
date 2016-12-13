c0065cbf51fe8e8ce080af1e08dbf13d03fd5b5c
Amos Boldor <amosboldor@gmail.com>
2016-12-12 16:26:23 -0800

    test pop of return value that was removed

--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -36,3 +36,14 @@ def test_size():
     linked_list.push('2')
     linked_list.push('3')
     assert linked_list.size() == 3
+
+
+def test_pop():
+    """Test pop method if removes the first value and return it."""
+    from linked_list import LinkedList
+    linked_list = LinkedList()
+    linked_list.push('1')
+    linked_list.push('2')
+    linked_list.push('3')
+    popped_item = linked_list.pop()
+    assert popped_item == '3'
