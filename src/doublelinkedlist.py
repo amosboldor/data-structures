693bb1787a89a58f7b1656683cc2eb9767b80139
Amos Boldor <amosboldor@gmail.com>
2016-12-14 12:16:16 -0800

    edited push method

--- src/doublelinkedlist.py
+++ src/doublelinkedlist.py
@@ -16,13 +16,22 @@ class DoubleLinkedList(object):
 
     def __init__(self, data=None):
         """Initialize DoubleLinkedList class with arguments."""
-        self.data = data
         self.head = None
         self.tail = None
         self.size = 0
+        if data:
+            try:
+                for item in data:
+                    self.push(item)
+            except TypeError:
+                self.head = Node(data)
 
     def push(self, val):
         """Push a node in initialized Double Linked List."""
         new_node = Node(val, next_item=self.head)
+        if self.size < 1:
+            self.tail = new_node
+        else:
+            self.head.prev_item = new_node
         self.head = new_node
         self.size += 1
