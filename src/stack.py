b964349688b0ad406dcf11cc322f1c8cc28dfb44
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-13 16:10:17 -0800

    created a Stack class

---
+++ src/stack.py
@@ -0,0 +1,10 @@
+"""Implement a Stack data type in Python."""
+
+from linked_list import LinkedList
+
+
+class Stack(object):
+    """Stack object for creating a stack list."""
+    
+    def __init__(self, data=None):
+    self.container = LinkedList(data)
