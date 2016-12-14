07867cbcdec8aed0dfd249ff662c472cd17c0d87
Amos Boldor <amosboldor@gmail.com>
2016-12-14 11:47:32 -0800

    created DoubleLinkedList Class

--- src/doublelinkedlist.py
+++ src/doublelinkedlist.py
@@ -5,7 +5,18 @@ class Node(object):
     """Make new data node."""
 
     def __init__(self, data=None, next_item=None, prev_item=None):
-        """Initialize Node with arguments."""
+        """Initialize Node class with arguments."""
         self.data = data
         self.next_item = next_item
         self.prev_item = prev_item
+
+
+class DoubleLinkedList(object):
+    """DoubleLinkedList class."""
+
+    def __init__(self, data=None):
+        """Initialize DoubleLinkedList class with arguments."""
+        self.data = data
+        self.head = None
+        self.tail = None
+        self.size = 0
