85d6e540866a9c1a81af1db2c1b8a694ee3558e8
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-22 20:52:14 -0800

    updated for queue

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-"""Setup mailroom madness module."""
+"""Setup datastructures module."""
 
 
 from setuptools import setup
 
 setup(
-    name="Linked Lists",
-    description="A Implementation a Singly-Linked List in Python",
+    name="Data structures",
+    description="The implementation of data structures in Python",
     version=0.1,
     author=["Claire Gatenby", "Amos Boldor"],
     licencse="MIT",
     package_dir={'': 'src'},
-    py_modules=["linked_list", "stack", "doublelinkedlist"],
+    py_modules=["linked_list", "stack", "doublelinkedlist", "queue"],
     extras_require={
         "test": ["pytest", "pytest-cov", "tox"]
     }
